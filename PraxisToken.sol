2e791cf3462a6b0a8f77548ee788f8604c092abc
Daniel Leonard <danielleonard24@gmail.com>
2021-09-22 16:15:18 -0400

    fixed some errors

--- PraxisToken.sol
+++ PraxisToken.sol
@@ -1,48 +1,70 @@
-[// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-
     function totalSupply() external view returns (uint256);
+
     function balanceOf(address account) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
 
-    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
     function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
-
 contract PraxisToken is IERC20 {
-
     string public constant name = "PraxisToken";
     string public constant symbol = "PRX";
-    uint8 public constant decimals = 18;  
+    uint8 public constant decimals = 18;
 
     mapping(address => uint256) balances;
 
-    mapping(address => mapping (address => uint256)) allowed;
-    
+    mapping(address => mapping(address => uint256)) allowed;
+
     uint256 totalSupply_;
 
-   constructor(uint256 total) {  
+    constructor(uint256 total) {
         totalSupply_ = total;
         balances[msg.sender] = totalSupply_;
-    }  
-
-    function totalSupply() public override view returns (uint256) {
-	    return totalSupply_;
     }
-    
-    function balanceOf(address tokenOwner) public override view returns (uint256) {
+
+    function totalSupply() public view override returns (uint256) {
+        return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        override
+        returns (uint256)
+    {
         return balances[tokenOwner];
     }
 
-    function transfer(address receiver, uint256 numTokens) public override returns (bool) {
+    function transfer(address receiver, uint256 numTokens)
+        public
+        override
+        returns (bool)
+    {
         require(numTokens <= balances[msg.sender]);
         balances[msg.sender] = balances[msg.sender] - numTokens;
         balances[receiver] = balances[receiver] + numTokens;
@@ -50,27 +72,37 @@ contract PraxisToken is IERC20 {
         return true;
     }
 
-    function approve(address delegate, uint256 numTokens) public override returns (bool) {
+    function approve(address delegate, uint256 numTokens)
+        public
+        override
+        returns (bool)
+    {
         allowed[msg.sender][delegate] = numTokens;
         emit Approval(msg.sender, delegate, numTokens);
         return true;
     }
 
-    function allowance(address owner, address delegate) public override view returns (uint) {
+    function allowance(address owner, address delegate)
+        public
+        view
+        override
+        returns (uint256)
+    {
         return allowed[owner][delegate];
     }
 
-    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
-        require(numTokens <= balances[owner]);    
+    function transferFrom(
+        address owner,
+        address buyer,
+        uint256 numTokens
+    ) public override returns (bool) {
+        require(numTokens <= balances[owner]);
         require(numTokens <= allowed[owner][msg.sender]);
-    
+
         balances[owner] = balances[owner] - numTokens;
         allowed[owner][msg.sender] = allowed[owner][msg.sender] - numTokens;
         balances[buyer] = balances[buyer] + (numTokens);
         emit Transfer(owner, buyer, numTokens);
         return true;
     }
-
-    
 }
-
