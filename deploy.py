f06770cbf1213798ca629ad44f2e1e83aa510a8e
Daniel Leonard <danielleonard24@gmail.com>
2021-09-22 16:14:11 -0400

    Started implementing compiling in py file

--- deploy.py
+++ deploy.py
@@ -1,2 +1,15 @@
+#Following tutuorial from FreecodeCamp.org with Patrick Collins 
+from solcx import compile_standard
+
 with open("./PraxisToken.sol", "r") as file:
     token_file = file.read()
+
+#compile solidity file using py-solc-x
+
+compiled_sol = compile_standard(
+    "language" : "Solidity",
+    "sources" : {"PraxisToken.sol": {"content":token_file}},
+    "setting" : {
+        
+    }
+)
